72cf596c838f1d98752b099de1606e6bd8de1efc
补档PO <25119890+BDPO@users.noreply.github.com>
2020-03-20 23:42:51 +0800

    Add files via upload

---
+++ AcFun-search-aide.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         AcFun搜索页 输入跳转ac号
+// @version      0.1
+// @description  详见 README 说明文档
+// @author       补档PO
+// @match        https://www.acfun.cn/search/*
+// @grant        none
+
+// ==/UserScript==
+
+window.open('https://www.acfun.cn/v/ac' + document.URL.split("ac")[2].split(";")[0]);void(0)
